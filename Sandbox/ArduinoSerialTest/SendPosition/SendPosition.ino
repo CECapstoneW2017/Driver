c5d210f9ad52f3867c0adde177b2d5b89a378ab1
Y2Que <jamesjr210@gmail.com>
2017-02-15 19:17:38 -0500

    IN PROGRESS: Receiving position data test
    Modified main and created a new arduino sketch. Arduino sends position data every 200ms, computer reads file every 1s. Trying to determine how to poll the arduino every 1 ms and only use the data when needed.

---
+++ Sandbox/ArduinoSerialTest/SendPosition/SendPosition.ino
@@ -0,0 +1,32 @@
+//Default baud speed for communication
+#define BAUD 9600
+//led 
+#define led 13
+//macro for on/off
+#define on (digitalWrite(led, HIGH))
+#define off (digitalWrite(led, LOW))
+
+void setup(){
+  Serial.begin(BAUD, SERIAL_8E1);
+  pinMode(led, OUTPUT);
+}
+
+void loop(){
+  String input;
+  //If any input is detected in arduino
+  if(Serial.available() > 0){
+    input = Serial.readStringUntil('\n');
+    //If input == "ON" then turn on the led 
+    //and send a reply
+    for(char i = 0; i < 50; i++){
+    if (input.equals("start")){
+      //generate random angles for testing
+      int thetaX = random(-31,31);
+      int thetaY = random(-31,31);
+      //send pos. values to computer
+      Serial.println(sprintf("thetaX: %l, thetaY: %l\n", thetaX, thetaY));
+      delay(200); //delay 200 ms
+    }
+    }
+  }
+}
--- Sandbox/ArduinoSerialTest/main.cpp
+++ Sandbox/ArduinoSerialTest/main.cpp
@@ -1,10 +1,11 @@
 #include "SerialPort.h"
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-char* portName = "\\\\.\\COM3";
+char* portName = "\\\\.\\COM6";
 char receivedString[MAX_DATA_LENGTH + 1];
 
 SerialPort *arduino;
@@ -18,7 +19,28 @@ int main(void)
 	bool sendResult = FALSE;
 
 	while (arduino->isConnected()) {
-		
+		//Test two: receive pos coordinates data from the arduino every 200ms, read the data every 1s 
+		cout << "Write \"start\": \n";
+		getline(cin, input_string);
+		char *c_string = new char[input_string.size() + 1];
+		int c_string_sz = input_string.size() + 1;
+
+		std::copy(input_string.begin(), input_string.end(), c_string);
+		c_string[input_string.size()] = '\n';
+		sendResult = arduino->writeSerialPort(c_string, c_string_sz); //CHANGED THE 2nd INPUT
+		for (char i = 0; i < 10; i++){
+			Sleep(1000);
+			readResult = arduino->readSerialPort(receivedString, MAX_DATA_LENGTH);
+			if (readResult != 0) {
+				receivedString[readResult] = 0;
+				cout << receivedString;
+			}
+			else {
+				cout << "Error: No data Received\n";
+			}
+		}
+		/*
+		Test one: write something to the terminal and the arduino will echo
 		cout << "Write something: \n";
 		getline(cin, input_string);
 		char *c_string = new char[input_string.size() + 1];
@@ -44,6 +66,7 @@ int main(void)
 			cout << "Error: No data sent\n";
 		}
 
-		delete[] c_string;
+		delete[] c_string;*/
+
 	}
 }
